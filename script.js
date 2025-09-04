19d673d38036472495b4c1761b48de1db8596ea2
Ankita Sontakke <121711590+ankitasontakke2001@users.noreply.github.com>
2025-09-04 19:05:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculate() {
+  let x = 5;
+  let y = 10;
+
+  // 👉 Set breakpoint here
+  let sum = addNumbers(x, y);
+
+  return sum;
+}
+function addNumbers(a, b) {
+  return a + b;
+}
+
+console.log(calculate());
